df495cc01c4a5c9475bffdee4eb0d72b23d2d5f3
MuriloCoelho01 <coelho.murilo10@gmail.com>
2025-03-22 13:32:18 -0300

    arrumando erro de concat

--- dados_m.py
+++ dados_m.py
@@ -2,10 +2,18 @@ import pandas as pd
 
 db_produtos = pd.read_excel('C:\\Users\\Danila\\Downloads\\CURSO PB BASES\\Tabela Produtos.xlsx')
 db_vendas = pd.read_excel('C:\\Users\\Danila\\Downloads\\CURSO PB BASES\\Tabela Vendas.xlsx')
+db_vendedores = pd.read_excel('C:\\Users\\Danila\\Downloads\\CURSO PB BASES\\Vendedor e seus IDs.xlsx')
 
 print(db_vendas)
 print(db_produtos)
+print(db_vendedores)
 
-#Manipulação da primeira base de dados
+
 cont_db = db_produtos.shape
 print(cont_db)
+
+
+nova_base = pd.concat([db_vendedores, db_produtos], axis=1, ignore_index = True)
+print(nova_base)
+
+
