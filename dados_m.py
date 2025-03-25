3967414d3918703768698e75382422cce1a27560
MuriloCoelho01 <coelho.murilo10@gmail.com>
2025-03-24 22:31:24 -0300

    fix

--- dados_m.py
+++ dados_m.py
@@ -17,3 +17,10 @@ nova_base = pd.concat([db_vendedores, db_produtos], axis=1, ignore_index = True)
 print(nova_base)
 
 
+nova_base[1] = nova_base[1].astype(str) 
+db_vendas['SKU'] = db_vendas['SKU'].astype(str)
+
+
+with pd.ExcelWriter('C:\\Users\\Danila\\Downloads\\CURSO PB BASES\\nova_base.xlsx') as writer:
+    nova_base.to_excel(writer, sheet_name='novabase', index=False)
+    
